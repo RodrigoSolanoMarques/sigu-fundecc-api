ba6bceefd4aa98ec032ade9668f12f009a0bb8f2
Rodrigo Solano Marques <>
2021-06-29 22:30:09 -0300

    v0.0.1 - ADICIONAR - Criar projeto.

---
+++ src/main/java/br/com/rodrigosolanomarques/sigufundecc/api/SiguFundeccApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigosolanomarques.sigufundecc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiguFundeccApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiguFundeccApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigosolanomarques/sigufundecc/api/SiguFundeccApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigosolanomarques.sigufundecc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiguFundeccApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
